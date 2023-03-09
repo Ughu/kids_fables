5c9e6c9667916c3c6e360b4671d3b22e4990edb6
madmolina <g.molina@shell.com>
2023-03-09 10:52:48 +0000

    improving model prompt

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import os
 openai.api_key_path = ".env"
 
 prompt = input("Choose words at random and I'll do my best to tell you a tale: ")
-print('Wait, your story is being created... \n\n')
+print('Wait, your story is being created... \n')
 response = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
     messages =[{'role': 'system', 'content': 'You tell stories for children. The stories you tell must have a message.'},
+               {'role': 'user', 'content': 'Remember: this stories are for children so they must have fantasy, adventure and they must teach about aspects of life'},
                {'role': 'user', 'content': prompt}])
 
 
