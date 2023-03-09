1c9d332de9afaa3d8e83a46c4e29d40e4b181d51
madmolina <g.molina@shell.com>
2023-03-09 10:36:09 +0000

    creating main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import openai
+import os
+
+openai.api_key_path = ".env"
+
+prompt = input("Choose words at random and I'll do my best to tell you a tale: ")
+print('Wait, your story is being created... \n\n')
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages =[{'role': 'system', 'content': 'You tell stories for children. The stories you tell must have a message.'},
+               {'role': 'user', 'content': prompt}])
+
+
+print(response.choices[0].message.content)
+print(f'\nThat wonderfull tale just costed you: '+str(int(response.usage.total_tokens)*0.002/1000)+' US dollars')
